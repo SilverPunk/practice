793f3721d632878abafdad9d5383182994194a08
SilverPunkN <34714326+SilverPunk@users.noreply.github.com>
2020-06-01 17:03:26 +0300

    Delete Atatat_1582301024.py

--- Atatat_1582301024.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Введіть число 1: "))
-b=int(input("Введіть число 2: "))
-c=int(input("Введіть число 3: "))
-if a!=b!=c:
-    print("0")
-elif a==b==c:
-    print("3")
-else:
-    print("2")
